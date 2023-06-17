a5b324abce896cee41513b777df36b75571516f6
Ayush Yadav <ayushyadav@Ayushs-MacBook-Air-2.local>
2023-06-17 05:44:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Skills} from './components/Skills'
+import { NavBar } from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Banner} from  './components/Banner'
+import { Services} from './components/Services';
+import {Contact} from './components/Contact'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Services />
+      <Contact />
+      
+
+
+
+
     </div>
   );
 }
---
+++ src/components/Services.js
@@ -0,0 +1,100 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import Temp from '../assets/img/temp.jpeg';
+import colorSharp2 from "../assets/img/color-sharp2.png";
+
+import TrackVisibility from 'react-on-screen';
+
+export const Services = () => {
+
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: Temp,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: Temp,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: Temp,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: Temp,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: Temp,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: Temp,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Services</h2>
+                <p>
+                    InterStellar-Security !!!!!!
+                </p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">PENTEST</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((Services, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...Services}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                        <p>
+                            InterStellar-Security
+                        </p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Defy Cyber Threats, Embrace Stellar Protection</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
